7f14aff0dbefaadbc3750b1ccf9fe8def3ff51ce
spainisnotequal <spainisnotequal@gmail.com>
2020-04-19 21:34:28 +0200

    Walk the tree

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
 const acorn = require("acorn");
+const walk = require("acorn-walk");
 const fs = require("fs");
 
 // read a file
@@ -7,4 +8,15 @@ const buffer = fs.readFileSync(FILE);
 
 // parse the file
 const ast = acorn.parse(buffer);
-console.log(ast);
+
+// simple walk looking for a literal
+walk.simple(ast, {
+  Literal(node) {
+    console.log(`Found a literal: ${node.value}.`);
+  },
+});
+
+// full walk, logging the type of nodes
+walk.full(ast, (node) => {
+  console.log(`There's a ${node.type} node.`);
+});
